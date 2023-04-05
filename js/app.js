13c5f371eef52cae0c22342ea14328f4352a788d
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-04-05 22:35:46 +0530

    weather app application

--- js/app.js
+++ js/app.js
@@ -1,54 +1,21 @@
-const apiKey = "ENTER YOUR API KEY";
+"use strict";
 
-const main = document.getElementById('main');
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-  
-const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+//Weather definition
+var Weather = function () {};
 
+Weather.prototype.setDate = function () {
+  var d = new Date();
+  var n = d.toDateString();
+  $("#date").text(n);
+};
 
-async function getWeatherByLocation(city){
-     
-         const resp = await fetch(url(city), {
-             origin: "cors" });
-         const respData = await resp.json();
-     
-           addWeatherToPage(respData);
-          
-     }
-
-      function addWeatherToPage(data){
-          const temp = Ktoc(data.main.temp);
-
-          const weather = document.createElement('div')
-          weather.classList.add('weather');
-
-          weather.innerHTML = `
-          <h2><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h2>
-          <small>${data.weather[0].main}</small>
-          
-          `;
-
-
-        //   cleanup 
-          main.innerHTML= "";
-           main.appendChild(weather);
-      };
-
-
-     function Ktoc(K){
-         return Math.floor(K - 273.15);
-     }
-
-
-
-     form.addEventListener('submit',(e) =>{
-        e.preventDefault();
-
-        const city = search.value;
-
-        if(city){
-            getWeatherByLocation(city)
-        }
-
-     });
+Weather.prototype.getLocation = function () {
+  //get location from ip address
+  var res = {
+    "city": "Buenos Aires",
+    "country": "Argentina",
+    "countryCode": "AR",
+    "query": "200.61.38.44",
+    "regionName": "Buenos Aires F.D.",
+    "status": "success"
+  };
